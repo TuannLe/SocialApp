87648407acc79f9cfada8bf42801a9a98190a757
Tuấn Lê <tuanle37372000@gmail.com>
2022-04-11 23:06:46 +0700

    add

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,21 @@
-export const getPosts = (req, res) => {
-    res.send('ROUTER SUCCESS')
+import { PostModel } from '../models/PostModel.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const post = new PostModel({
+            title: 'test',
+            content: 'test'
+        })
+        post.save()
+
+        const posts = await PostModel.find()
+        console.log('posts', posts)
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Create SUCCESS')
 }
